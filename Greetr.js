508153b178b7c05004546273dd2542400a0d3858
GusVilla303 <gusvilla303@gmail.com>
2015-10-02 23:25:14 -0600

    Fix camel case naming on parameters firstName and lastName

--- Greetr.js
+++ Greetr.js
@@ -1,16 +1,37 @@
 (function(global, $){
-  var Greetr = function(firstname, lastname, language) {
-    return new Greetr.init(firstname, lastname, language);
+  var Greetr = function(firstName, lastName, language) {
+    return new Greetr.init(firstName, lastName, language);
   }
 
-  Greetr.prototype = {};
+  var supportedLangs = ['en', 'es'];
+
+  var greetings = {
+      en: 'Hello',
+      es: 'Hola'
+  };
+
+  var formalGreetings = {
+    en: 'Greetings',
+    es: 'Saludos'
+  };
+
+  var logMessages = {
+    en: 'Logged in',
+    es: 'Inicio sesion'
+  };
+
+  Greetr.prototype = {
+    fullname: function() {
+      return this.firstName + ' ' + this.lastName;
+    }
+  };
 
 
-  Greetr.init = function(firstname, lastname, language) {
+  Greetr.init = function(firstName, lastName, language) {
 
     var self = this;
-    self.firstname = firstname || '';
-    self.lastname  = lastname  || '';
+    self.firstName = firstName || '';
+    self.lastName  = lastName  || '';
     self.language  = language  || 'en';
 
   }
